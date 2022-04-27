a9063ccddda1a1243b3ec53e965ac6ffae49261e
chadjmoulton <chadjmoulton@gmail.com>
2022-04-26 23:56:23 -0400

    more work to algorithm and comments

--- Sudoku/Board.cs
+++ Sudoku/Board.cs
@@ -17,29 +17,53 @@ namespace Sudoku
 
         private char[,] Solve(char[,] board)
         {
+            //defaults to decoy if no valid board
             char[,] decoy = new char[,] { { 'a', 'a' } };
 
+            //loop through individual cells to find empty
             for (int i = 0; i < 9; i++)
             {
                 for (int j = 0; j < 9; j++)
                 {
-
+                    if (board[i, j] == 'X') //if empty
+                    {
+                        for (char num = '1'; num <= '9'; num++) //loop through possible entries
+                        {
+                            if (IsCorrect(board, i, j, num)) //check if entry is valid
+                            {
+                                board[i, j] = num;
+                            }
+                        }
+                    }
                 }
             }
         }
 
-        static bool In3by3(char[,] board, int row, int col, char cell)
+        //confirm valid number placement by checking 3x3 cell, row, and column
+        static bool IsCorrect(char[,] board, int row, int col, char num)
         {
-            bool exists = false;
+            bool correct = true;
+
             for (int i = 0; i < 9; i++)
             {
-                if (board[3 * (row / 3) + i / 3, 3 * (col / 3) + i % 3] != 'X' && board[3 * (row / 3) + i / 3, 3 * (col / 3) + i % 3] == cell)
+                //confirm number not in 3x3 cell
+                if (board[3 * (row / 3) + i / 3, 3 * (col / 3) + i % 3] != 'X' && board[3 * (row / 3) + i / 3, 3 * (col / 3) + i % 3] == num)
                 {
-                    exists = true;
+                    correct = false;
                 }
-                else exists = false;
+                //confirm number not in row
+                if (board[i, col] != 'X' && board[i, col] == num)
+                {
+                    correct = false;
+                }
+                //confirm number not in column
+                if (board[row, i] != 'X' && board[row, i] == num)
+                {
+                    correct = false;
+                }
+                else correct = true;
             }
-            return exists;
+            return correct;
         }
     }
 }
